264ed180371640d4cab03cdfef4bc58e3684935d
tangramteam <tangramteam@outlook.com>
2023-05-03 19:54:20 +0800

    代码维护

--- Sample/MFCComponrnt/CosmosComponent/CosmosApp.h
+++ Sample/MFCComponrnt/CosmosComponent/CosmosApp.h
@@ -1,5 +1,5 @@
 /********************************************************************************
- *           Web Runtime for Application - Version 1.0.1.202110220001           *
+ *           AIGC for Desktop Application - Version 1.0.1.202110220001           *
  ********************************************************************************
  * Copyright (C) 2002-2021 by Tangram Team.   All Rights Reserved.
  *
