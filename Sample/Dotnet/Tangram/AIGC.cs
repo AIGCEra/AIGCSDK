17aef788cbd43e368b516231bd591be297a2ff49
tangramteam <tangramteam@outlook.com>
2024-04-22 05:56:17 +0800

    20240422 edition

--- Sample/Dotnet/Tangram/AIGC.cs
+++ Sample/Dotnet/Tangram/AIGC.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
-
+using System.Security.Cryptography;
+using System.Text;
 namespace AIGC
 {
     static public class AIGCApp
     {
+        static IntPtr initDll = IntPtr.Zero;
         [DllImport("kernel32.dll")]
         public static extern IntPtr LoadLibrary(string dllToLoad);
 
@@ -13,10 +17,61 @@ namespace AIGC
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate void InitWebRT(IntPtr IUnkPtr, IntPtr IUnkWndPtr);
+        static string BuildConfigDataFile(string strExeName, string strProductName, string strCompanyPathName)
+        {
+            string _strProductName = strProductName.ToLower();
+            string _strCompanyPathName = strCompanyPathName.ToLower();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData)).Append("\\TangramData\\").Append(strExeName).Append("\\");
+            string _strConfigDataFile = sb.ToString().ToLower();
+            if (!Directory.Exists(_strConfigDataFile))
+                Directory.CreateDirectory(_strConfigDataFile);
+            using (var md5 = MD5.Create())
+            {
+                StringBuilder sb2 = new StringBuilder();
+                _ = sb2.Append(_strConfigDataFile).Append("@").Append(_strCompanyPathName).Append("@").Append(_strProductName);
+                var result = md5.ComputeHash(Encoding.UTF8.GetBytes(sb2.ToString()));
+                _strConfigDataFile += BitConverter.ToString(result).Replace("-", "");
+            }
+            _strConfigDataFile += "\\";
+            if (!Directory.Exists(_strConfigDataFile))
+                Directory.CreateDirectory(_strConfigDataFile);
+            _strConfigDataFile += strExeName;
+            _strConfigDataFile += ".tangram";
+            return _strConfigDataFile;
+        }
 
         public static bool InitCosmos(object StartObj, object MainWndObj = null)
         {
-            IntPtr initDll = LoadLibrary(@"universe.DLL");
+            string strCfgDataFile = BuildConfigDataFile("aigcbrowser", "aigcbrowser", "Tangram Team");
+            string strData = File.ReadAllText(strCfgDataFile);
+            string strUniverseFilePath = "";
+            //string strChromeRTFilePath = "";
+            int nPos = strData.IndexOf("Universe");
+            if (nPos != -1)
+            {
+                String strTemp = strData.Substring(nPos + 8);
+                nPos = strTemp.IndexOf(".dll");
+                strTemp = strTemp.Substring(0, nPos + 4);
+                nPos = strTemp.IndexOf(":");
+                strUniverseFilePath = strTemp.Substring(nPos - 1);
+                if (!File.Exists(strUniverseFilePath)) { strUniverseFilePath = ""; }
+            }
+            //nPos = strData.IndexOf("AIGCAgent");
+            //if (nPos != -1)
+            //{
+            //    String strTemp = strData.Substring(nPos + 9);
+            //    nPos = strTemp.IndexOf(".dll");
+            //    strTemp = strTemp.Substring(0, nPos + 4);
+            //    nPos = strTemp.IndexOf(":");
+            //    strChromeRTFilePath = strTemp.Substring(nPos - 1);
+            //    if (!File.Exists(strChromeRTFilePath)) { strChromeRTFilePath = ""; }
+            //}
+            initDll = LoadLibrary(@"universe.DLL");
+            if (initDll == IntPtr.Zero && strUniverseFilePath != "")
+            {
+                initDll = LoadLibrary(strUniverseFilePath);
+            }
             if (initDll != IntPtr.Zero)
             {
                 IntPtr fnInitWebRT = GetProcAddress(initDll, "InitWebRT");
